ad5a268cb10da47a1e602bc9cbd619cb5adec42a
Aalekh-Sajonia <aalekhbh@gmail.com>
2020-03-15 13:40:13 +0530

    Bug Fix

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, {lazy,Suspense} from 'react';
 
+import ErrorBoundary from './components/error-boundary/error-boundary.component';
+import Spinner from './components/spinner/spinner.component';
 import { GlobalStyle } from './global.styles';
-import ShopPage from './pages/shop/shop.component'
 import {Switch, Route, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {setCurrentUser} from './redux/user/user.actions'; 
-import HomePage from './pages/homepage/homepage.component';
-import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { selectCurrentUser } from './redux/user/user.selector';
 import {createStructuredSelector} from 'reselect';
-import SignInAndSiignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import CheckoutPage from './pages/checkout/checkout.component';
 import { selectCollectionsForPreview } from './redux/shop/shop.selector';
+import Header from './components/header/header.component';
+
+const HomePage = lazy(() => import('./pages/homepage/homepage.component'));
+const ShopPage = lazy(() => import('./pages/shop/shop.component'));
+const SignInAndSiignUpPage = lazy(() => import('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component'));
+const CheckoutPage = lazy(() => import('./pages/checkout/checkout.component'));
 
 class App extends React.Component {
 
@@ -49,11 +52,15 @@ class App extends React.Component {
       <GlobalStyle />
       <Header/>
       <Switch>
+      <ErrorBoundary>
+      <Suspense fallback={<Spinner/>}>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
           <Route exact path='/checkout' component={CheckoutPage} />
           <Route exact path='/signin' render={()=>this.props.currentUser ? 
             (<Redirect to='/' />) : (<SignInAndSiignUpPage/>)} />
+      </Suspense>
+      </ErrorBoundary>
       </Switch>
           
       </div>
